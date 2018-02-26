19b581cad057e2dc390bd29cdb9b6a17513f3038
DavidIsCool <davexiao2002@gmail.com>
2018-02-25 19:13:06 -0500

    Create A_Plus_B.cpp

---
+++ A_Plus_B.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+    int d;
+    int num1;
+    int num2;
+    cin >> d;
+    for (int i = 0; i < d; i++) {
+        cin >> num1;
+        cin >> num2;
+        cout << num1 + num2 << "\n";
+    }
+    return 0;
+}
