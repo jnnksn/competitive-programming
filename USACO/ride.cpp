3dd2946c4bd45b9dd5d10ac2a54fff373bc48660
DavidIsCool <davexiao2002@gmail.com>
2018-03-02 16:43:47 -0500

    Create ride.cpp

---
+++ USACO/ride.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string a, b;
+
+int main() {
+    freopen("ride.in", "r", stdin);
+    freopen("ride.out", "w", stdout);
+    int z = 1, y = 1;
+    cin >> a >> b;
+    for (int x = 0; x < a.length(); x++) z *= a[x] - 'A'+1;
+    for (int x = 0; x < b.length(); x++) y *= b[x] - 'A'+1;
+    
+    
+    if (z % 47 == y % 47) cout << "GO" << "\n";
+    else cout << "STAY" << "\n";
+    return 0;
+}
+
