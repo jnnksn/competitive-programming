a32182c092100f8b175d6fa555b60045c31d95aa
DavidIsCool <davexiao2002@gmail.com>
2018-03-15 18:14:28 -0400

    Create CCC16_J3.cpp

---
+++ CCC/CCC16_J3.cpp
@@ -0,0 +1,32 @@
+// https://dmoj.ca/problem/ccc16j3
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+using namespace std;
+string word;
+string c;
+int C = 0;
+vector<string> track;
+void find_pal(string N) {
+    
+    if (N == string(N.rbegin(), N.rend())) {
+        if (N.length() > C) C = N.length();
+    } else {
+        if(find(track.begin(), track.end(), N) != track.end()) {
+        } else {
+            track.push_back(N);
+            find_pal(N.substr(1));
+            find_pal(N.substr(0, N.length()-1));
+        }
+            
+        
+    }
+}
+
+int main() {
+    cin >> word;
+    find_pal(word);
+    cout << C;
+}
