cce7acb145b26b9a96ac4d6cdab994a22088bcab
DavidIsCool <davexiao2002@gmail.com>
2018-04-17 21:24:18 -0400

    Create CCC01_S5.py

---
+++ CCC/2001/CCC01_S5.py
@@ -0,0 +1,46 @@
+# https://dmoj.ca/problem/ccc01s5
+# 04/17/2018
+# 30/40 on DMOJ ~ Time Limit Exceeded
+
+limit = int(input())
+N = int(input())
+a = []
+b = []
+num_index = []
+
+for _ in range(N):
+    a.append(input())
+for i in range(N):
+    b.append(input())
+    num_index.append(i)
+
+retVal = 0
+retIn = []
+
+def find(x, y, k, index):
+    global retVal
+    global retIn
+    
+    if x == y:
+        retVal = k
+        retIn = index.split()
+        return True
+    elif k >= limit:
+        return False
+    elif x not in y and y not in x:
+        return False
+    else:
+        for i in num_index:
+            d = x + a[i]
+            ka = y + b[i]
+            find(d,ka, k+1, index + " " + str(i+1))
+
+for i in num_index:
+    find(a[i], b[i], 1, str(i+1))
+
+if retVal > 0 and len(retIn) != 0:
+    print(retVal)
+    for j in retIn:
+        print(j)       
+else:
+    print("No solution.")
