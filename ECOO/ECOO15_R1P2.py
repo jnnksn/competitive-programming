dee98f8601f4d2c9624305096d8b03fc65de6a41
DavidIsCool <davexiao2002@gmail.com>
2018-04-27 19:14:53 -0400

    +5!

--- ECOO/ECOO15_R1P2.py
+++ ECOO/ECOO15_R1P2.py
@@ -1,18 +1,20 @@
 # https://dmoj.ca/problem/ecoo15r1p2
 # 4/27/2018
 # ----------------------------------
-# 70/100 Marks on DMOJ because I suck.
+# 75/100 Marks on DMOJ because I suck.
 # ----------------------------------
 
 for _ in range(10):
     W = int(input())
     words = input().split()
 
-    current = 0
-    line = ''
-   
     for i in words:
-        if current + len(i)+1 <= W+1:
+        if len(line) > W+1:
+            while(len(line) > W):
+                print(line[0:W])
+                line = line[W:]
+            line += i + ' '
+        elif current + len(i)+1 <= W+1:
             line += i + ' '
             current += len(i)+1
         else:
@@ -20,9 +22,12 @@ for _ in range(10):
             if len(i) > W:
                 print(i[0:W])
                 line = i[W:] + ' '
+                current = len(i[W:]) + 1
             else:
                 line = i + ' '
-            current = len(i)+1
+                current = len(i)+1
         if i == words[-1]:
             print(line)
     print('=====')
+        
+
