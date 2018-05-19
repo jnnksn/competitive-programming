85faeffcaf86ff284b97bbb7191e475b2588bcd9
DavidIsCool <davexiao2002@gmail.com>
2018-05-19 13:33:38 -0400

    Update CCC16_S4.cpp

--- CCC/2016/CCC16_S4.cpp
+++ CCC/2016/CCC16_S4.cpp
@@ -56,7 +56,7 @@ bool find_two(int fs, int le) {
     return false;
 }
 
-// FS <= first block section => FE | MS <= middle block section => ME | LS <= end block section => LE
+// FS <= first block section => FE | MS <= middle block section => ME | LS <= last block section => LE
 // If total of all blocks in first section == total of all blocks in last section. We can combine.
 void find_three(int fs, int le) {
     REP(ms, fs+1, le) {
