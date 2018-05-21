309c9f9ff26ebdeb47c3966cc8ab1f9bb0c8375d
DavidIsCool <davexiao2002@gmail.com>
2018-05-21 19:53:13 -0400

    Create CCC17_S2.py

---
+++ CCC/2017/CCC17_S2.py
@@ -0,0 +1,20 @@
+// https://dmoj.ca/problem/ccc17s2
+// 05/21/2018
+
+import math
+N = int(input())
+tides = [int(i) for i in input().split()]
+tides.sort()
+
+split = math.ceil(N/2)
+low = tides[0:split]
+low.sort(reverse=True)
+high = tides[split:]
+
+s = ""
+for i in range(len(low)):
+    s += str(low[i]) + " "
+    if N%2 == 0 or i != len(low)-1:
+        s += str(high[i]) + " "
+        
+print(s)
