091ef48c2866e20870b13fa3aa37351ce3689b40
DavidIsCool <davexiao2002@gmail.com>
2018-06-26 17:17:11 -0400

    Update CCC18_J4S2.cpp

--- CCC/2018/CCC18_J4S2.cpp
+++ CCC/2018/CCC18_J4S2.cpp
@@ -1,4 +1,25 @@
 // https://dmoj.ca/problem/ccc18s2
+// -------------------------------
+// Most solutions go with a mathematical approach, however I solved it differently.
+// We notice that the indexes in the 2d table change in a pattern when we rotate.
+// 1 2 3
+// 4 5 6
+// 7 8 9
+//
+// when rotated once:
+//
+// 3 6 9
+// 2 5 8
+// 1 4 7
+//
+// Notice how the first row becomes the first column
+// the second row becomes the second column
+// and the third row becomes the third column
+//
+// Thus, knowing this we can simply check if the table is correct based on the criteria given, and if not
+// we rotate and check again.
+// this continues until we eventually find the correct table (max 4 times since 90 + 90 + 90 + 90 = 360, and we repeat after that)
+// -------------------------------
 #include <bits/stdc++.h>
 #include <iostream>
 using namespace std;
