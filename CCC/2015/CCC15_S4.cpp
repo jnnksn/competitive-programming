2cfe118d60ef787aa89e446b2c5e889e282e6f46
DavidIsCool <davexiao2002@gmail.com>
2018-06-29 13:41:23 -0400

    Update CCC15_S4.cpp

--- CCC/2015/CCC15_S4.cpp
+++ CCC/2015/CCC15_S4.cpp
@@ -1,6 +1,23 @@
 // https://dmoj.ca/problem/ccc15s4
 // 06/29/2018
-// -------------------------------
+// -----------------------------------------------------------------------
+// The Algorithm:
+// We first notice that we must find the shortest path from island A to B, with no negative weights (time > 0)
+// Thus, we are able to use Dijkstra's Algorithm to solve this problem.
+// This would be a direct implementation of Dijkstra, but the problem throws in another variable --> hull damage
+// The only impact this variable has is that we will be using a 2D distance and visited array!
+// Since, we are no longer keeping track of just the distance from node X to Y, but
+// the distance from node X with hull damage i to node Y with hull damage i+h (where h is how much the hull is damaged
+// going from X to Y).
+// Therefore, we can use dists[i][j], where i represents the node and j the amount of damage that has been taken.
+// For example:
+// Going from island 5 to island 6, we take 5 damage to our hull, and it takes 10 minutes.
+// The distance can be represented as: dists[6][i+5] = dists[5][i] + 10, where i is the previous amount of damage at island 5. 
+// So, as long as i+h < K, it is possible.
+
+// Finally, loop through dists[E][i], where E is the end node and i is the damage taken from 0 to K-1, keeping track
+// of the smallest distance.
+// -----------------------------------------------------------------------
 
 #include <iostream>
 #include <queue>
@@ -24,18 +41,15 @@ void solve() {
 
 	dists[S][0] = 0;
 	q.push({0, {0, S}});
-    
 	while(!q.empty()) {
 		int a = q.top().second.second, z = q.top().second.first; q.pop();
 		if (visited[a][z]) continue;
 		visited[a][z] = true;
 		for (auto u : adj[a]) {
 			int x = u.first, w = u.second.first, d = u.second.second;
-			for (int i = 0; i < K-d; ++i) {
-				if (dists[a][i]+w < dists[x][i+d]) {
-					dists[x][i+d] = dists[a][i] + w;
-					q.push({-dists[x][i+d], {i+d, x}});
-				}
+			if (z+d < K && dists[a][z]+w < dists[x][z+d]) {
+				dists[x][z+d] = dists[a][z] + w;
+				q.push({-dists[x][z+d], {z+d, x}});
 			}
 		}
 	}
